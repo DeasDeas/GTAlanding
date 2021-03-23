4649881cb454a1deec399362bb0857822c2d4e1c
Михаил <mikenic30@gmail.com>
2021-03-23 14:01:30 +0300

    startpoint

---
+++ js/auth.js
@@ -0,0 +1,18 @@
+
+
+function authAction(event) {
+	const login = document.getElementById('login').value;
+	const password = document.getElementById('password').value;
+
+	if (login === "admin" && password === "123") {
+		window.history.pushState({}, "", '/lk.html');
+		document.location.reload();
+	}
+	else {
+		document.querySelector('.error').style.visibility = "visible";
+	}
+	event.preventDefault();
+}
+
+const form = document.querySelector('.auth-form');
+form.addEventListener('submit', authAction)
---
+++ js/scroll.js
@@ -0,0 +1,35 @@
+const header = document.querySelector('header')
+
+const main = document.getElementById("main-btn");
+const start = document.getElementById("start-btn");
+const shop = document.getElementById("shop-btn");
+
+window.addEventListener("scroll", (event) => {
+	let scroll = this.scrollY;
+
+	if (scroll !== 0) {
+		header.style.background = "#2f51cc7a";
+	}
+	else {
+		header.style.background= "var(--bg-color-light)";
+	}
+
+	if (scroll > 920 && scroll < 1500) {
+		main.className = "button-like"
+		start.className = "button-like"
+		shop.className = "button-like"
+		start.classList.add("button-like__selected")
+	}
+	else if (scroll > 1500) {
+		main.className = "button-like"
+		start.className = "button-like"
+		shop.className = "button-like"
+		shop.classList.add("button-like__selected")
+	}
+	else {
+		main.className = "button-like"
+		start.className = "button-like"
+		shop.className = "button-like"
+		main.classList.add("button-like__selected")
+	}
+});
